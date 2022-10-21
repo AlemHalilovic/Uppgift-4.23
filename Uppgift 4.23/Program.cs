3e3f966fe5f5671c684123e1673729311938396c
TheBlueSoul5188 <hibba04@hotmail.se>
2022-10-21 11:02:27 +0200

    Add project files.

---
+++ Uppgift 4.23/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Uppgift_4_23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ett heltal");
+            int tal = int.Parse(Console.ReadLine());
+
+            int Largest = tal;
+            string svar = "";
+            while (svar != "Nej")
+            {
+                Console.WriteLine("Skulle du vilja skriva in ett till heltal?(Ja/Nej)");
+                svar = Console.ReadLine();
+
+                if (svar == "Nej")
+                {
+                    break;
+
+                }
+                Console.WriteLine("Skriv in ditt tal här");
+                int nyttHeltal  = int.Parse(Console.ReadLine());
+                if (nyttHeltal  > Largest)
+                {
+                    Largest  = nyttHeltal;
+
+                }
+                Console.WriteLine($"Det största talet du skrev var {Largest}");
+            }
+
+        }
+    }
+}
+
